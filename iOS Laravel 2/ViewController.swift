e9e354736c920a275e9bd81796567295e9449354
suebtas <suebtas@gmail.com>
2016-10-09 01:26:43 +0700

    Initial Commit

---
+++ iOS Laravel 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Laravel 2
+//
+//  Created by Suebtas on 10/9/2559 BE.
+//  Copyright © 2559 Suebtas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
